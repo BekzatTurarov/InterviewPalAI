edd55bafc9618b691935d73e77312138555b03e1
BekzatTurarov <turarovb0810@gmail.com>
2023-08-06 17:18:51 +0600

    changes

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -6,11 +6,11 @@ const LandingPage = () => {
       <header>
         <nav className="bg-white border-white-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="http://localhost:5173/#" className="flex items-center">
+            <a className="flex items-center">
               <img
                 src="logo.png"
                 className="mr-3 h-6 sm:h-9 rounded-full"
-                alt="Flowbite Logo"
+                alt="Logo"
               />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 InterviewPal
