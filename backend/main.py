dc3caebe569e2a20eb343fde2428423ca3def600
BekzatTurarov <turarovb0810@gmail.com>
2023-08-06 20:31:09 +0600

    FINAL

--- backend/main.py
+++ backend/main.py
@@ -26,10 +26,7 @@ app = FastAPI()
 
 # CORS - Origins
 origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:4173",
-    "http://localhost:3000",
+    "https://interview-pal-ai.onrender.com"
 ]
 
 
