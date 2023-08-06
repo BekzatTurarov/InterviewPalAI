ac7323cc7aba1e7de8a80b3d13dc0d4f38d70f91
BekzatTurarov <turarovb0810@gmail.com>
2023-08-06 20:24:18 +0600

    changes_final

--- frontend/src/components/UploadPage.tsx
+++ frontend/src/components/UploadPage.tsx
@@ -6,12 +6,12 @@ const UploadPage: React.FC = () => {
       <header>
         <nav className="bg-white border-white-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="http://localhost:5173/" className="flex items-center">
-              <img
+            <a href="/" className="flex items-center">
+              {/* <img
                 src="logo.png"
                 className="mr-3 h-6 sm:h-9 rounded-full"
                 alt="Flowbite Logo"
-              />
+              /> */}
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 InterviewPal
               </span>
